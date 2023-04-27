c61706d8e1d67819a5fa51317273352896a2b90e
Сергей Ковальский <237264@mail.ru>
2023-04-27 16:52:03 +0300

    change name get_weather

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def show_weather(some_city):
+def get_weather(some_city):
     url = "https://ru.wttr.in/{}?n?M?q?T".format(some_city)
     response = requests.get(url)
     response.raise_for_status()
@@ -11,4 +11,4 @@ def show_weather(some_city):
 if __name__ == '__main__':
     cities = ("Лондон", "аэропорт Шереметьево", "Череповец")
     for city in cities:
-        print(show_weather(city))
+        print(get_weather(city))
